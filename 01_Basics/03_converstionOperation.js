e8e0957a80a7b8b045a53bb128db8a372c498ca6
ZainNaeem-Official <zainnaeemOfficial@gmail.com>
2024-04-27 14:38:37 +0500

    Operations discussed

--- 01_Basics/03_converstionOperation.js
+++ 01_Basics/03_converstionOperation.js
@@ -32,6 +32,46 @@ String to Boolean
 
 let someNumber = 19;
 let stringNumber = String(someNumber);
-console.log(stringNumber); 
-console.log(typeof stringNumber); 
+// console.log(stringNumber); 
+// console.log(typeof stringNumber); 
+
+// ***************** Operation ********************//
+
+let value = 3; 
+let negVal = -value;
+// console.log(negVal);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2/2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2%3);
+
+let str1 = "Hello";
+let str2 = " Zain";
+
+let str3 = console.log(str1 + str2);
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log((1 + 2) * 5 % 2 );
+
+// console.log(true);
+// console.log(+true); //bad practice 
+// console.log(+""); //bad practice 
+
+let num1, num2, num3;
+num1 = num2 = num3 = 5 ;  
+
+
+let gameCounter = 100; 
+// console.log(gameCounter++); 
+// console.log(gameCounter);
+
 
