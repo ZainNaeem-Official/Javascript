31059299e50c970f590e74f4965e50144d8089bc
ZainNaeem-Official <zainnaeemOfficial@gmail.com>
2024-04-27 13:28:52 +0500

    Operations discussed

---
+++ 01_Basics/03_converstionOperation.js
@@ -0,0 +1,37 @@
+let score = "zain";
+
+// console.log(typeof score);
+
+let valueInNumber = Number (score);
+// console.log(typeof (valueInNumber));
+// console.log(valueInNumber);
+
+/*
+
+Converting string to number 
+"33"=> 33
+"33abc" => NaN
+"true" => 1, "false" => 0
+
+Converting to number 
+null = > 0 , undefine => NaN
+
+*/
+
+let isLoggedIn = "";
+
+let booleanIsLoggedIn = Boolean (isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+/*
+String to Boolean
+"" => false
+"zain" => true
+*/
+
+
+let someNumber = 19;
+let stringNumber = String(someNumber);
+console.log(stringNumber); 
+console.log(typeof stringNumber); 
+
