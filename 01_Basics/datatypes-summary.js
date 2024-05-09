fb682cb4466401f3161a0c5a3f27df34c54948c0
ZainNaeem-Official <zainnaeemOfficial@gmail.com>
2024-05-09 07:11:50 +0500

    stack and heap

--- 01_Basics/datatypes-summary.js
+++ 01_Basics/datatypes-summary.js
@@ -12,7 +12,7 @@ let userEmail;
 const id = Symbol('123')
 const anotherId = Symbol('123')
 
-console.log(id === anotherId);
+// console.log(id === anotherId);
 
 // const bigNumber = 3456543576654356754n
 
@@ -32,6 +32,30 @@ const myFunction = function(){
     console.log("Hello world");
 }
 
-console.log(typeof anotherId);
+// console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+// ++++++++++++++++++++++++++++++++++++++++
+// Stack(Primitive)  Heap(Non-Primitive)
+
+let myYoutubeName = "hitestChoudharydotcom";
+let anothername = myYoutubeName; 
+anothername = "ChaiAurCode";
+
+// console.log(anothername);
+// console.log(myYoutubeName);
+
+let userOne = {
+    email : "user@google.com",
+    upi : "user@ybl.com"
+}
+
+let userTwo = userOne;
+userTwo.email = "hitesh@google.com";
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+
+
