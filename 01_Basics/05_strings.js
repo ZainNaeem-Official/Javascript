65be83a2268fafd683f6da0c1314aebdd1e959c2
ZainNaeem-Official <zainnaeemOfficial@gmail.com>
2024-05-09 08:40:30 +0500

    strings

---
+++ 01_Basics/05_strings.js
@@ -0,0 +1,26 @@
+const name = "Zain";
+const repoCount = 50;
+
+// console.log(name + repoCount + "Value");
+// console.log(`hello my name is ${name.toUpperCase()} and my RepoCount is ${repoCount}`);
+
+let gameName = new String('Muhammad Zain Naeem');
+// console.log(typeof gameName);
+// console.log(gameName.split(" ", 1)); // seprator, limit
+// console.log(gameName[8]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(3));
+// console.log(gameName.indexOf("n"));
+// console.log(gameName.substring(0,4));
+// console.log(gameName.slice(-5,-1));
+// console.log(gameName.trim());
+// console.log(gameName.trim().length);
+
+const url = "https://hitest.com/hitesh%20choudhary"
+// console.log(url.replace("%20", " "))
+// console.log();
+// console.log(url.includes("%20"));
